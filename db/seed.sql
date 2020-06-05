60f092e8d03db5ba24547920175dd1d94c59bb73
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-04 21:36:31 -0400

    added seed.sql with tables

---
+++ db/seed.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT,
+    department_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY KEY (id)
+)
