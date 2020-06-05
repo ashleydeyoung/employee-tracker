1fa4e6fcddb02b40442fb4e9f84c1c04a371076d
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-04 21:26:16 -0400

    added console.table package, starter inquirer questions

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
-  host: "localhost",
+// const connection = mysql.createConnection({
+//   host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+//   // Your port; if not 3306
+//   port: 3306,
 
-  // Your username
-  user: "root",
+//   // Your username
+//   user: "root",
 
-  // Your password
-  password: "rootroot",
-  database: "playlistDB"
-});
+//   // Your password
+//   password: "rootroot",
+//   database: "playlistDB",
+// });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-
-});
+// connection.connect(function (err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId);
+//   startApp();
+// });
+startApp()
+function startApp() {
+  inquirer.prompt({
+    type: "list",
+    message: "What would you like to do?",
+    name: "mainMenu",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add departments",
+      "Add roles",
+      "Add employees",
+      "Update employee role",
+      "EXIT"
+    ],
+  })
+  .then (function(answer) {
+      console.log(answer)
+      // if answer then function answer
+      //if exit connection.end()
+  })
+}
